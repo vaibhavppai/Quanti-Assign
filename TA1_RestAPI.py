b891d07f2b17e2b5958b1ed0105e26603bbeb0b5
Vaibhav Pai <vaibhavppai@gmail.com>
2019-11-09 19:00:47 +0530

    Added routes for getting movie details and sorting

--- TA1_RestAPI.py
+++ TA1_RestAPI.py
@@ -11,3 +11,16 @@ movies_db = pd.read_excel("Movies.xlsx")
 
 # Converting to SQL database
 movies_db.to_sql(name='Movies',con = cnx)
+
+@route('/getMovie')
+@auth_basic(is_authenticated_user)
+def getMovieDetails(name):
+    details = pd.read_sql("SELECT movie, year, imdb, duration, description FROM Movies WHERE movie='" + name + "'", con = cnx)
+    return details.to_string()
+
+@route('/sortBy', method = ['POST', 'GET'])
+@auth_basic(is_authenticated_user)
+def sortBy(field, type='asc'):
+    sorted = pd.read_sql("SELECT movie, year, imdb, duration, description FROM Movies ORDER BY "+field+", movie "+type,con = cnx)
+    print(sorted['year'])
+    return sorted.to_string()
